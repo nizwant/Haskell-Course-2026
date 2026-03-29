c307fa0935ef6c7b5410205f7bcb68984fca08f6
nizwant <mateusznizwantowski@gmail.com>
2026-03-29 12:50:21 +0200

    feat: 1,2,3 homework 2

---
+++ homeworks/hw2/Solution.hs
@@ -0,0 +1,42 @@
+import GHC.Real (underflowError)
+
+data Sequence a = Empty | Single a | Append (Sequence a) (Sequence a)
+
+-- Task 1
+instance Functor Sequence where
+  fmap :: (a -> b) -> Sequence a -> Sequence b
+  fmap _ Empty = Empty
+  fmap f (Single x) = Single (f x)
+  fmap f (Append x y) = Append (fmap f x) (fmap f y)
+
+-- Task 2
+instance Foldable Sequence where
+  foldMap :: (Monoid m) => (a -> m) -> Sequence a -> m
+  foldMap _ Empty = mempty
+  foldMap f (Single x) = f x
+  foldMap f (Append x y) = foldMap f x <> foldMap f y
+
+seqToList :: Sequence a -> [a]
+seqToList = foldr (:) []
+
+seqLength :: Sequence a -> Int
+seqLength = foldl (\c _ -> c + 1) 0
+
+-- Task 3
+
+instance Semigroup (Sequence a) where
+  (<>) :: Sequence a -> Sequence a -> Sequence a
+  seq1 <> seq2 = Append seq1 seq2
+
+instance Monoid (Sequence a) where
+  mempty :: Sequence a
+  mempty = Empty
+
+-- Task 4
+-- Task 5
+-- Task 6
+-- Task 7
+-- Task 8
+
+main :: IO ()
+main = putStrLn "Hello"
